f8aefe0ba7c78c99052426cdc019cbb83857a3ea
Jeff Wright <jeff42.wright@gmail.com>
2023-12-30 16:45:18 -0600

    v0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.9.21"
     `maven-publish`
+    id("com.ncorti.ktfmt.gradle") version "0.11.0"
 }
 
 repositories {
@@ -15,8 +16,15 @@ dependencies {
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
     implementation("com.google.apis:google-api-services-sheets:v4-rev20220927-2.0.0")
 
+    //implementation("com.google.auth:google-auth-library-credentials:1.19.0")
+    implementation("com.google.auth:google-auth-library-oauth2-http:1.19.0")
+
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+ktfmt {
+    googleStyle() // 2-space indentation
 }
 
 tasks.test {
@@ -35,13 +43,18 @@ compileTestKotlin.kotlinOptions {
     jvmTarget = "11"
 }
 
+java {
+    // Publish Sources
+    withSourcesJar()
+}
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
 
             groupId = "com.github.JeffWright"
-            version = "0.1.0"
-            artifactId = "google-sheets"
+            version = "0.2.0"
+            artifactId = "kt-sheets"
 
             from(components["java"])
         }
--- src/main/kotlin/dev/jtbw/googlesheets/GoogleSheets.kt
+++ src/main/kotlin/dev/jtbw/googlesheets/GoogleSheets.kt
@@ -1,165 +1,240 @@
 package dev.jtbw.googlesheets
 
-import com.google.api.client.auth.oauth2.Credential
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.googleapis.json.GoogleJsonError
 import com.google.api.client.googleapis.json.GoogleJsonResponseException
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.gson.GsonFactory
-import com.google.api.client.util.store.FileDataStoreFactory
 import com.google.api.services.sheets.v4.Sheets
-import com.google.api.services.sheets.v4.SheetsScopes
+import com.google.api.services.sheets.v4.model.ClearValuesRequest
+import com.google.api.services.sheets.v4.model.ClearValuesResponse
 import com.google.api.services.sheets.v4.model.UpdateValuesResponse
 import com.google.api.services.sheets.v4.model.ValueRange
+import com.google.auth.http.HttpCredentialsAdapter
+import com.google.auth.oauth2.GoogleCredentials
 import java.io.File
-import java.io.FileNotFoundException
-import java.io.IOException
-import java.io.InputStreamReader
-import java.security.GeneralSecurityException
 
-
-private object SheetsApi {
-    private const val APPLICATION_NAME = "Google Sheets API"
+class GoogleSheetsService(private val service: Sheets) {
+  companion object {
     private val JSON_FACTORY: JsonFactory = GsonFactory.getDefaultInstance()
-    private const val TOKENS_DIRECTORY_PATH = "tokens"
-
-    val service: Sheets by lazy {
-        val HTTP_TRANSPORT: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport()
-        Sheets.Builder(
-            HTTP_TRANSPORT,
-            JSON_FACTORY,
-            getCredentials(HTTP_TRANSPORT)
-        )
-            .setApplicationName(APPLICATION_NAME)
-            .build()
-    }
 
     /**
-     * Global instance of the scopes required by this quickstart.
-     * If modifying these scopes, delete your previously saved tokens/ folder.
+     * @param credentialsJsonFile: To create a service account and get keys for it, go to:
+     * https://console.cloud.google.com/iam-admin/serviceaccounts
      */
-    private val SCOPES = listOf(SheetsScopes.SPREADSHEETS)
-    private const val CREDENTIALS_FILE_PATH = "/sheets_credentials.json"
+    fun create(
+      applicationName: String,
+      credentialsJsonFile: File,
+    ): GoogleSheetsService {
+      require(credentialsJsonFile.exists()) {
+        "File not found: ${credentialsJsonFile.absolutePath}"
+      }
 
-    /**
-     * Creates an authorized Credential object.
-     *
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
-    @Throws(IOException::class)
-    private fun getCredentials(HTTP_TRANSPORT: NetHttpTransport): Credential {
-        // Load client secrets.
-        val `in` = SheetsApi::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
-            ?: throw FileNotFoundException("Resource not found: $CREDENTIALS_FILE_PATH -- place file under /resources \n" +
-                    "get it from https://console.cloud.google.com/apis/credentials \n" +
-                    "see also: https://developers.google.com/sheets/api/quickstart/java")
+      val httpTransport: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport()
+      val googleCredentials: GoogleCredentials =
+        GoogleCredentials.fromStream(credentialsJsonFile.inputStream())
 
-        val clientSecrets: GoogleClientSecrets =
-            GoogleClientSecrets.load(
-                JSON_FACTORY, InputStreamReader(`in`)
-            )
+      val service: Sheets =
+        Sheets.Builder(httpTransport, JSON_FACTORY, HttpCredentialsAdapter(googleCredentials))
+          .setApplicationName(applicationName)
+          .build()
 
-        // Build flow and trigger user authorization request.
-        val flow: GoogleAuthorizationCodeFlow =
-            GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES
-            )
-                .setDataStoreFactory(
-                    FileDataStoreFactory(
-                        File(
-                            TOKENS_DIRECTORY_PATH
-                        )
-                    )
-                )
-                .setAccessType("offline")
-                .build()
-        val receiver: LocalServerReceiver = LocalServerReceiver.Builder().setPort(8888).build()
-        return AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
+      return GoogleSheetsService(service)
     }
+  }
 
-    /**
-     * Prints the names and majors of students in a sample spreadsheet:
-     * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-     */
-    @Throws(IOException::class, GeneralSecurityException::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
-    }
+  fun getSheet(
+    spreadsheetId: String,
+    sheetName: String = "Sheet1",
+  ): GoogleSheet {
+    return GoogleSheet(spreadsheetId, sheetName, service)
+  }
 }
 
-class GoogleSheet(
-    val spreadsheetId: String,
-    val sheetName: String = "Sheet1",
+class GoogleSheet
+internal constructor(
+  val spreadsheetId: String,
+  val sheetName: String,
+  private val service: Sheets
 ) {
-    fun readRange(
-        /** e.g. "A2:E" */
-        range: String
-    ): List<List<String>> {
-        // Build a new authorized API client service.
-        val sheetAndRange = "$sheetName!$range"
-        val service = SheetsApi.service
-        val response: ValueRange = service.spreadsheets().values()
-            .get(spreadsheetId, sheetAndRange)
-            .execute()
-        return response.getValues()
-            ?.map {
-                it.map { it.toString() }
-            }
-            ?: emptyList()
-    }
+  companion object {
+    const val FULL_SHEET = "A1:ZZ"
+  }
 
-    fun writeRange(
-        /** e.g. "A2:E" */
-        range: String,
-        values: List<List<Any?>>,
-        parseInput: Boolean = false
-    ) {
-        val valueInputOption: String = if (parseInput) "USER_ENTERED" else "RAW"
-        var result: UpdateValuesResponse? = null
-        try {
-            // Updates the values in the specified range.
-            val body = ValueRange()
-                .setValues(values)
-            result = SheetsApi.service.spreadsheets().values().update(spreadsheetId, range, body)
-                .setValueInputOption(valueInputOption)
-                .execute()
-            println("${result.updatedCells} cells updated.")
-        } catch (e: GoogleJsonResponseException) {
-            val error: GoogleJsonError = e.details
-            if (error.code == 404) {
-                println("Spreadsheet not found with id '$spreadsheetId'.")
-            }
+  /** @param range e.g. "A2:E" */
+  fun readRange(range: String = FULL_SHEET): List<List<String>> {
+    // Build a new authorized API client service.
+    val sheetAndRange = "$sheetName!$range"
+    val response: ValueRange =
+      service.spreadsheets().values().get(spreadsheetId, sheetAndRange).execute()
+    return response.getValues()?.map { it.map { it.toString() } } ?: emptyList()
+  }
 
-            throw e
+  /** Assumes first row is field names, will pass each [createRow] a map of fieldName -> contents */
+  fun <T> readRange(range: String = FULL_SHEET, createRow: (Map<String, String>) -> T): List<T> {
+    val data = readRange(range)
+    val fields = data.first()
+    val rows = data.drop(1)
+
+    return rows.map { row ->
+      val paddedRow = buildList {
+        addAll(row)
+        while (size < fields.size) {
+          add("")
         }
+      }
+      val zipped = fields.zip(paddedRow).toMap()
+      createRow(zipped)
     }
+  }
 
-    fun <T> readData(range: String, vararg selectedColumns: String, block: (List<String?>) -> T) : List<T> {
-        val data = readRange(range)
-        val columnNames = data.first()
-            .map { it.lowercase() }
-        val columnIdxs = selectedColumns
-            .map { selected ->
-                columnNames.indexOf(selected.lowercase())
-                    .takeIf { it >=0 }
-                    ?: throw IllegalArgumentException("No column named $selected in $sheetName.  Columns were: $columnNames")
-            }
+  /**
+   * @param range e.g. "A2:E"
+   * @param parseInput true if you want "=1+A2" to turn into a formula, false if you want it to be a
+   * string
+   */
+  fun writeRange(
+    values: List<List<Any?>>,
+    range: String = FULL_SHEET,
+    parseInput: Boolean = false
+  ) {
+    val sheetAndRange = "$sheetName!$range"
+    val valueInputOption: String = if (parseInput) "USER_ENTERED" else "RAW"
+    var result: UpdateValuesResponse? = null
+    try {
+      // Updates the values in the specified range.
+      val body = ValueRange().setValues(values)
+      result =
+        service
+          .spreadsheets()
+          .values()
+          .update(spreadsheetId, sheetAndRange, body)
+          .setValueInputOption(valueInputOption)
+          .execute()
+      println("${result.updatedCells ?: 0} cells updated.")
+    } catch (e: GoogleJsonResponseException) {
+      val error: GoogleJsonError = e.details
+      if (error.code == 404) {
+        println("Spreadsheet not found with id '$spreadsheetId'.")
+      }
 
-        return data
-            .drop(1) // headers
-            .map { rowAsList ->
-                block(
-                    columnIdxs.map { idx ->
-                        rowAsList.getOrNull(idx)// ?: throw IllegalArgumentException("No data for ")
-                    }
-                )
-            }
+      throw e
     }
+  }
+
+  /**
+   * Assumes first row is field names, will write each value's entries under the corresponding
+   * columns.  Fields are _not_ case-sensitive
+   * @param appendUnknownFields should fields present in the data but not in the Sheet be added to
+   * the Sheet?
+   */
+  fun writeRange(values: List<Map<String, Any?>>, appendUnknownFields: Boolean = true) {
+    val fields = (readRange("A1:ZZ1").firstOrNull() ?: emptyList())
+      .map { Field(it) }
+    // println("DBG: fields = $fields")
+    val unknownFields = buildSet {
+      values.forEach { addAll(it.keys.map { Field(it) }) }
+      fields.forEach { remove(it) }
+    }
+    // println("DBG: unknown = $unknownFields")
+
+    val fieldsToWrite = if(appendUnknownFields) fields + unknownFields else fields
+    val processedValues =
+      values.map { value ->
+        val fieldMap = value.mapKeys { (k, _) -> Field(k) }
+        fieldsToWrite.map { field ->
+          // null will be skipped by Google Sheets API, we want blank instead
+          fieldMap[field] ?: ""
+        }
+      }
+
+    clearRange("A2:ZZ")
+    writeRange(
+      values = processedValues,
+      range = "A2:ZZ",
+    )
+
+    if(appendUnknownFields) {
+      val col = columnIndexToLetter(fields.size + 1)
+      writeRange(listOf(unknownFields.map { it.name }), range = "${col}1:ZZ1")
+    }
+  }
+
+  fun clearRange(
+    range: String = FULL_SHEET,
+  ) {
+    val sheetAndRange = "$sheetName!$range"
+    var result: ClearValuesResponse? = null
+    try {
+      // Updates the values in the specified range.
+      // val body = ValueRange()
+      //    .setValues(values)
+      val request = ClearValuesRequest()
+      result =
+        service
+          .spreadsheets()
+          .values()
+          .clear(spreadsheetId, sheetAndRange, request)
+          // .update(spreadsheetId, sheetAndRange, body)
+          // .setValueInputOption(valueInputOption)
+          .execute()
+      println("${sheetAndRange} cleared.")
+    } catch (e: GoogleJsonResponseException) {
+      val error: GoogleJsonError = e.details
+      if (error.code == 404) {
+        println("Spreadsheet not found with id '$spreadsheetId'.")
+      }
+
+      throw e
+    }
+  }
+
+  fun <T> readData(
+    range: String = FULL_SHEET,
+    vararg selectedColumns: String,
+    block: (List<String?>) -> T
+  ): List<T> {
+    val sheetAndRange = "$sheetName!$range"
+    val data = readRange(sheetAndRange)
+    val columnNames = data.first().map { it.lowercase() }
+    val columnIdxs =
+      selectedColumns.map { selected ->
+        columnNames.indexOf(selected.lowercase()).takeIf { it >= 0 }
+          ?: throw IllegalArgumentException(
+            "No column named $selected in $sheetName.  Columns were: $columnNames"
+          )
+      }
+
+    return data
+      .drop(1) // headers
+      .map { rowAsList ->
+        block(
+          columnIdxs.map { idx ->
+            rowAsList.getOrNull(idx) // ?: throw IllegalArgumentException("No data for ")
+          }
+        )
+      }
+  }
 }
+
+private data class Field(val name: String) {
+
+  override fun toString(): String = "<$name>"
+
+  override fun equals(other: Any?): Boolean {
+    val otherField = other as? Field ?: return false
+    return name.equals(otherField.name, true)
+  }
+
+  override fun hashCode(): Int {
+    return name.lowercase().hashCode()
+  }
+}
+
+/* 1 -> A */
+// TODO: only supports through Z
+private fun columnIndexToLetter(idx: Int): String {
+  return ('A'..'Z').drop(idx-1).first().toString()
+}
+
